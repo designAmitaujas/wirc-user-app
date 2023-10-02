a28b93645ad618f5c115b60c03cf20b4f66066d2
Bhavyamandviya <bhavya17mandviya@gmail.com>
2023-10-02 16:15:05 +0530

    cc

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,8 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://occasional-person-mime-lite.trycloudflare.com/graphql",
+  // schema: "https://occasional-person-mime-lite.trycloudflare.com/graphql",
+  schema: "https://admin.wirc-icai.org/graphql",
   documents: "src/graphql/**/*.graphql",
   generates: {
     "src/gql/graphql.tsx": {

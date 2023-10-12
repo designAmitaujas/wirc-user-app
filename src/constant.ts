88ba28a61f59794eb4365115ab112534d50db0df
Bhavyamandviya <bhavya17mandviya@gmail.com>
2023-10-12 12:08:27 +0530

    cc

--- src/constant.ts
+++ src/constant.ts
@@ -3,7 +3,7 @@ import Url from "url-parse";
 
 export const GQL_API_URL =
   //  "https://admin.wirc-icai.org/graphql";
-  "https://home-within-argument-consoles.trycloudflare.com/graphql";
+  "https://word-former-sheffield-gods.trycloudflare.com/graphql";
 
 const parsedUrl = new Url(GQL_API_URL);
 

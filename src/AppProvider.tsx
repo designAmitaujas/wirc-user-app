4c2221eda25c74f6ee173b23a78fa0260f4e99ec
designAmitaujas <design@amitaujas.com>
2023-12-10 16:42:44 +0530

    cg

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,85 +1,129 @@
 import messaging from "@react-native-firebase/messaging";
+import * as Notifications from "expo-notifications";
 import * as SplashScreen from "expo-splash-screen";
 import { FC, ReactNode, useEffect } from "react";
-import { Alert } from "react-native";
+import { PermissionsAndroid } from "react-native";
 import { useSetFirebaseIdMutation } from "./gql/graphql";
 import { useAppAuthState } from "./store";
 
 SplashScreen.preventAutoHideAsync();
 
-// if (!firebase.app.length) {
-//   firebase.initializeApp({
-//     apiKey: "AIzaSyCqJWuH3XrQOVVP5eaOX2lTZay_k-2TmD4",
-//     // authDomain: "YOUR_AUTH_DOMAIN",
-
-//     projectId: "wirc-1b868",
-//     storageBucket: "wirc-1b868.appspot.com",
-//     messagingSenderId: "703855841112",
-//     appId: "1:703855841112:android:9ad8298647e7c4b09c9f2d",
-//     databaseURL: "https://wirc.firebaseio.com",
-//     androidClientId: "com.amitaujas.WIRCUser",
-//     clientId: "109064105829116229778",
-//   });
-// }
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 const AppProvider: FC<{ children: ReactNode }> = ({ children }) => {
-  const [response] = useSetFirebaseIdMutation();
+  const [setFirebaseToken] = useSetFirebaseIdMutation();
   const { isAuth } = useAppAuthState();
-  const requestUserPermission = async () => {
-    const authStatus = await messaging().requestPermission();
-    const enabled =
-      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
-      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
-
-    console.log(authStatus);
-
-    if (enabled) {
-      console.log("Authorization status:", authStatus);
-    }
-  };
 
   useEffect(() => {
-    //@ts-ignore
-    if (requestUserPermission()) {
-      //return fcm token for the device
-      messaging()
-        .getToken()
-        .then((token) => {
-          if (isAuth) {
-            response({ variables: { firebaseId: token } });
-          }
-          console.log(token);
-        });
-    } else {
-      console.log("Failed token status");
-    }
-
-    // Check whether an initial notification is available
-    messaging()
-      .getInitialNotification()
-      .then(async (remoteMessage) => {
-        if (remoteMessage) {
-          console.log(
-            "Notification caused app to open from quit state:",
-            remoteMessage.notification
+    (async () => {
+      try {
+        if (isAuth === true) {
+          PermissionsAndroid.request(
+            PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS
           );
+
+          Notifications.setNotificationChannelAsync("default", {
+            name: "default",
+            importance: Notifications.AndroidImportance.MAX,
+            vibrationPattern: [0, 250, 250, 250],
+            lightColor: "#FF231F7C",
+          });
+
+          const { status: existingStatus } =
+            await Notifications.getPermissionsAsync();
+          let finalStatus = existingStatus;
+
+          if (existingStatus !== "granted") {
+            const { status } = await Notifications.requestPermissionsAsync();
+            finalStatus = status;
+          }
+
+          if (finalStatus !== "granted") {
+            return;
+          }
+
+          Notifications.setNotificationHandler({
+            handleNotification: async () => ({
+              shouldShowAlert: true,
+              shouldPlaySound: true,
+              shouldSetBadge: false,
+            }),
+          });
+
+          await messaging().setAutoInitEnabled(true);
+
+          const authStatus = await messaging().requestPermission();
+
+          const enabled =
+            authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+            authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+          if (enabled) {
+            const token = await messaging().getToken();
+
+            console.log(token);
+
+            await setFirebaseToken({ variables: { firebaseId: token } });
+
+            messaging().onNotificationOpenedApp(async (remoteMessage) => {
+              console.log(
+                "Notification caused app to open from background state:",
+                remoteMessage.notification
+              );
+            });
+
+            await messaging()
+              .getInitialNotification()
+              .then((remoteMessage) => {
+                if (remoteMessage) {
+                  console.log(
+                    "Notification caused app to open from quit state:",
+                    remoteMessage.notification
+                  );
+                }
+              });
+
+            // Register background handler
+            messaging().setBackgroundMessageHandler(async (remoteMessage) => {
+              console.log(
+                "Message handled in the background!",
+                remoteMessage.data
+              );
+
+              Notifications.scheduleNotificationAsync({
+                content: {
+                  // @ts-ignore
+                  title: remoteMessage.data.title,
+                  // @ts-ignore
+                  body: remoteMessage.data.body,
+                },
+                trigger: null,
+              });
+            });
+          }
         }
-      });
-
-    messaging().onNotificationOpenedApp(async (remoteMessage) => {
-      console.log(
-        "Notification caused app to open from background state:",
-        remoteMessage.notification
-      );
-    });
-
-    // Register background handler
-    messaging().setBackgroundMessageHandler(async (remoteMessage) => {
-      console.log("Message handled in the background!", remoteMessage);
-    });
+      } catch (err) {
+        console.log("ERRR");
+      }
+    })();
 
     const unsubscribe = messaging().onMessage(async (remoteMessage) => {
-      Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+      // Alert.alert("A new FCM message arrived!", JSON.stringify(remoteMessage));
+      Notifications.scheduleNotificationAsync({
+        content: {
+          // @ts-ignore
+          title: remoteMessage.data.title,
+          // @ts-ignore
+          body: remoteMessage.data.body,
+        },
+        trigger: null,
+      });
     });
 
     return unsubscribe;

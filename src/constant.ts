6053eb7ede896db74470c0afdc6bcbfb31c0f6cf
Bhavyamandviya <bhavya17mandviya@gmail.com>
2023-12-27 18:45:19 +0530

    cc

--- src/constant.ts
+++ src/constant.ts
@@ -1,9 +1,9 @@
 import { Dimensions } from "react-native";
 import Url from "url-parse";
 
-// export const GQL_API_URL = "https://admin.wirc-icai.org/graphql";
-export const GQL_API_URL =
-  "https://affecting-sci-pubmed-ot.trycloudflare.com/graphql";
+export const GQL_API_URL = "https://admin.wirc-icai.org/graphql";
+// export const GQL_API_URL =
+//   "https://phantom-zoo-levels-space.trycloudflare.com/graphql";
 
 const parsedUrl = new Url(GQL_API_URL);
 
